9a64379fa5a550749a79cfafff20f7c9e552cf36
dhruva-star88 <dhruvatej6565@gmail.com>
2024-02-24 23:16:48 +0530

    plotted grap using plotly

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this project about?
+This is project about whether forecast data, main this is user interactive web app.
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import streamlit as st
-
+import plotly.express as px
 
 st.title("Weather Forecast for the Next Days")
 place = st.text_input(label="Place", placeholder="Type The City Name...", key="place")
 days = st.slider(label="Forecast Days", min_value=1, max_value=5, help="Select the number of days")
-option = st.selectbox(label="Select data to view", options=["Temperature", "Sky"], placeholder="Select Any of the Option", index=None)
+option = st.selectbox(label="Select data to view", options=["Temperature", "Sky"],
+                      placeholder="Select Any of the Option", index=None)
 st.subheader(f"{option} for the next {days} days in {place}")
 
+
+# Here the following function returns two items and it will be stored in tuple, so we use two variables to access the two items in tuple in two diff. variable
+
+
+def get_plot_data(days):
+    date = ["56", "13", "12"]
+    temp = [51, 12, 45]
+    temp = [days * i for i in temp]
+    return date, temp
+
+
+d, t = get_plot_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
+st.plotly_chart(figure)
